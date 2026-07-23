7a4bb6a7470b06dc375bb1e4bbc3a3398c4898ea
Hemant Birla <birlahemant.hb.b19@gmail.com>
2026-07-23 11:28:04 +0530

    feat(client): add protected routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,35 +2,57 @@ import React from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import AuthLayout from "../layouts/AuthLayout";
+
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
 
+import PublicRoute from "./PublicRoute";
+import ProtectedRoute from "./ProtectedRoute";
+
+// Temporary placeholder until Dashboard is built
+const Dashboard = () => <h1>Dashboard</h1>;
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthLayout />,
+    element: <Navigate to="/login" replace />,
+  },
+
+  {
+    element: <PublicRoute />,
     children: [
       {
-        index: true, // 💡 Redirects root '/' directly to '/login'
-        element: <Navigate to="/login" replace />,
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/login",
+            element: <Login />,
+          },
+          {
+            path: "/register",
+            element: <Register />,
+          },
+          {
+            path: "/forgot-password",
+            element: <ForgotPassword />,
+          },
+          {
+            path: "/reset-password",
+            element: <ResetPassword />,
+          },
+        ],
       },
+    ],
+  },
+
+  {
+    element: <ProtectedRoute />,
+    children: [
       {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "reset-password",
-        element: <ResetPassword />,
+        path: "/dashboard",
+        element: <Dashboard />,
       },
     ],
   },
