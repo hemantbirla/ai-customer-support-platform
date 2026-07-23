c9332449b38984b2dbf69a569be33b7ece75690c
Hemant Birla <birlahemant.hb.b19@gmail.com>
2026-07-23 11:16:45 +0530

    feat(client): implement auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
+import { AuthProvider } from "./context/AuthContext";
 
 import router from "./routes/AppRoutes";
 
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </>
+
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+    </AuthProvider>
   );
 }
 
